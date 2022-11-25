bae02d7b75d89c8ad86764f21f28d84fb4dc42e1
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2022-11-25 22:38:28 +0200

    integrate passport

---
+++ src/middleware/passport.ts
@@ -0,0 +1,50 @@
+import passport from 'passport';
+import { Strategy as LocalStrategy } from 'passport-local';
+import bcrypt from 'bcrypt';
+import { UserRepo } from '../db/userRepo';
+import { UserTypes } from '../models/user';
+
+const User = new UserRepo();
+const PEPPER = process.env.PEPPER as string;
+
+passport.use(new LocalStrategy({ usernameField: 'email' }, async (email, password, done) => {
+    try {
+        console.log('xxxx')
+        const user = await User.read({ email });
+        console.log(user);
+        if (!user) {
+            // no error has occurred, but the user does not exist
+            return done(null, false);
+        }
+        else if (user) {
+            const password_match = await bcrypt.compare(password+PEPPER, user.password)
+            console.log(password_match);
+            // no error has occurred, but the password is incorrect
+            if (password_match)
+                return done(null, user);
+        }
+        // no error has occurred, and the user exists and the password is correct
+        return done(null, false);
+    } catch (err) {
+        // an error has occurred
+        return done(err);
+    }
+}))
+
+passport.serializeUser((user: UserTypes, done) => {
+    done(null, user.id);
+})
+
+passport.deserializeUser(async (u: UserTypes, done) => {
+    try {
+        const user = await User.read({ id: u.id})
+        if (!user) {
+            return done(null, false);
+        }
+        return done(null, user);
+    } catch (err) {
+        return done(err);
+    }
+})
+
+export default passport;

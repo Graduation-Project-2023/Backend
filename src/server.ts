ca76a48da3c2dcb247e6954bb1dae4dbaf65b404
Saif Abuelhasan <saiofa99@gmail.com>
2022-11-26 01:31:41 +0200

    solved login error

--- src/server.ts
+++ src/server.ts
@@ -2,34 +2,34 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import passport from "passport";
-import session from 'express-session';
+import session from "express-session";
 import router from "./routes/index";
-import { PrismaSessionStore } from '@quixo3/prisma-session-store';
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
 import { PrismaClient } from "@prisma/client";
 
 dotenv.config();
 const port = process.env.PORT || 3000;
 
 const app = express();
+const prisma = new PrismaClient();
 app.use(express.json());
 
 // mandatory for passport in order to work
-app.use(session({
+app.use(
+  session({
     secret: process.env.SESSION_SECRET as string,
-    resave: false, 
+    resave: false,
     saveUninitialized: false,
     cookie: {
-        maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
     },
-    store: new PrismaSessionStore(
-      new PrismaClient(),
-      {
-        checkPeriod: 2 * 60 * 1000,  //ms
-        dbRecordIdIsSessionId: true,
-        dbRecordIdFunction: undefined,
-      }
-    )
-}))
+    store: new PrismaSessionStore(prisma, {
+      checkPeriod: 2 * 60 * 1000, //ms
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  })
+);
 
 app.use(cors());
 app.use(passport.initialize());
@@ -39,11 +39,11 @@ import { UserRepo } from "./db/userRepo";
 
 const user = new UserRepo();
 import bcrypt from "bcrypt";
-require('dotenv').config();
+require("dotenv").config();
 const PEPPER = process.env.PEPPER as string;
 const passwo = 123456;
 app.get("/", (req: express.Request, res: express.Response) => {
-  const pass = bcrypt.hashSync(passwo+PEPPER, 13);
+  const pass = bcrypt.hashSync(passwo + PEPPER, 13);
   user.create({
     email: "mohamed.raafat@eng.suez.edu.eg",
     password: pass,

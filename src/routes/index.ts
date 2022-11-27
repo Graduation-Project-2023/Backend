f78e94a2e910d0c4915f2144d6a95abd836aeed9
Saif Abuelhasan <saiofa99@gmail.com>
2022-11-28 00:18:33 +0200

    export routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,11 @@
 import express, { Request, Response } from "express";
 import server from "./api/auth";
-import adm_server from "./api/admission";
+import admission from "./api/admission";
+import program from "./api/program";
+import course from "./api/course";
+import grade from "./api/grade";
+import level from "./api/level";
+import programCourse from "./api/programCourse";
 import errors from "../error/errHandler";
 
 const router = express.Router();
@@ -10,7 +15,8 @@ router.get("/", (req: Request, res: Response) => {
 });
 
 router.use("/", server);
-router.use("/", adm_server);
+router.use("/", admission);
+
 router.use((err: any, req: Request, res: Response, next: any) => {
   if (err.message && err.message in Object.keys(errors)) {
     return res.status(errors[err.message]).json({ error: err.message });

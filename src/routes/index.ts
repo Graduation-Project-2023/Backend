a3fdfec8f21da11dad5fb3568d22838b922764e9
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2022-11-28 00:14:46 +0200

    edit server names

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from "express";
-import server from "./api/auth";
-import adm_server from "./api/admission";
+import { auth_server } from "./api/auth";
+import { admission_server } from "./api/admission";
 import errors from "../error/errHandler";
 
 const router = express.Router();
@@ -9,8 +9,8 @@ router.get("/", (req: Request, res: Response) => {
   res.send("API router is working");
 });
 
-router.use("/", server);
-router.use("/", adm_server);
+router.use("/", auth_server);
+router.use("/", admission_server);
 router.use((err: any, req: Request, res: Response, next: any) => {
   if (err.message && err.message in Object.keys(errors)) {
     return res.status(errors[err.message]).json({ error: err.message });

f01fbcbc8ad548994a584a1df1e1d80137a3cdfb
Saif Abuelhasan <saiofa99@gmail.com>
2022-12-17 20:35:53 +0200

    get course instances by program

--- src/controllers/courseInstanceController.ts
+++ src/controllers/courseInstanceController.ts
@@ -9,10 +9,23 @@ export class CourseInstanceController extends Controller {
 
   getAll = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const programId = req.params.program_id as string;
+      const academicSemesterId = req.params.semester_id as string;
       const levelId = req.params.level_id as string;
       const data = await this.model.getAll({
-        AND: [{ programId }, { levelId }],
+        AND: [{ academicSemesterId }, { levelId }],
+      });
+      res.status(200).json(data);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  getAllByProgram = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const academicSemesterId = req.params.semester_id as string;
+      const programId = req.params.program_id as string;
+      const data = await this.model.getAll({
+        AND: [{ academicSemesterId }, { programId }],
       });
       res.status(200).json(data);
     } catch (error) {

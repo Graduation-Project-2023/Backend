1711f5b5bc2833a353c3f840a09e0f47a041b67a
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2022-12-21 15:55:47 +0200

    student and user model

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,6 +1,40 @@
-export interface UserTypes {
-  id?: string;
-  email?: string;
-  password?: string;
-  role?: string;
+import { Prisma } from "@prisma/client";
+import prisma from "../db";
+
+export class User {
+  static getAll = async () => {
+    const data = await prisma.user.findMany({});
+    return data;
+  };
+
+  static get = async (id: string) => {
+    const data = await prisma.user.findUnique({
+      where: { id },
+    });
+    return data;
+  };
+
+  static create = async (data: any) => {
+    const user = await prisma.user.create({
+      data: {
+        ...data,
+      },
+    });
+    return user;
+  };
+
+  static update = async (id: string, data: Prisma.UserCreateInput) => {
+    const user = await prisma.user.update({
+      where: { id },
+      data,
+    });
+    return user;
+  };
+
+  static delete = async (id: string) => {
+    const user = await prisma.user.delete({
+      where: { id },
+    });
+    return user;
+  };
 }
